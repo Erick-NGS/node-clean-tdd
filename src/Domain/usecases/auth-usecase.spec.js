7367cd2f59680d9c529fc0dd4da9864410213f27
Erick Silva <erick.silva@firstdecision.com.br>
2021-05-14 10:09:19 -0300

    added test to ensure the repo is called with the right email

--- src/Domain/usecases/auth-usecase.spec.js
+++ src/Domain/usecases/auth-usecase.spec.js
@@ -1,6 +1,10 @@
 const { MissingParamError } = require('../../utils/errors')
 
 class AuthUseCase {
+  constructor (loadUserEmailOnRepo) {
+    this.loadUserEmailOnRepo = loadUserEmailOnRepo
+  }
+
   async auth (email, password) {
     if (!email) {
       throw new MissingParamError('email')
@@ -9,6 +13,7 @@ class AuthUseCase {
     if (!password) {
       throw new MissingParamError('password')
     }
+    await this.loadUserEmailOnRepo.load(email)
   }
 }
 
@@ -25,5 +30,19 @@ describe('Auth UseCase', () => {
     const promise = sut.auth('mail.test@mail.com')
 
     expect(promise).rejects.toThrow(new MissingParamError('password'))
+  })
+
+  test('Should call LoadUserEmailOnRepo with correct email', async () => {
+    class LoadUserEmailOnRepoSpy {
+      async load (email) {
+        this.email = email
+      }
+    }
+
+    const loadUserEmailOnRepoSpy = new LoadUserEmailOnRepoSpy()
+    const sut = new AuthUseCase(loadUserEmailOnRepoSpy)
+    await sut.auth('mail.test@mail.com', 'dsdfs')
+
+    expect(loadUserEmailOnRepoSpy.email).toBe('mail.test@mail.com')
   })
 })
