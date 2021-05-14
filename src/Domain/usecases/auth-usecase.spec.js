06420d00833eeda2a53397ed3cf5a48249e4e996
Erick Silva <erick.silva@firstdecision.com.br>
2021-05-14 10:13:23 -0300

    refactoring auth-usecase.spec

--- src/Domain/usecases/auth-usecase.spec.js
+++ src/Domain/usecases/auth-usecase.spec.js
@@ -17,30 +17,36 @@ class AuthUseCase {
   }
 }
 
+const makeSut = () => {
+  class LoadUserEmailOnRepoSpy {
+    async load (email) {
+      this.email = email
+    }
+  }
+
+  const loadUserEmailOnRepoSpy = new LoadUserEmailOnRepoSpy()
+  const sut = new AuthUseCase(loadUserEmailOnRepoSpy)
+
+  return { sut, loadUserEmailOnRepoSpy }
+}
+
 describe('Auth UseCase', () => {
   test('Should throw if there\'s no email', async () => {
-    const sut = new AuthUseCase()
+    const { sut } = makeSut()
     const promise = sut.auth()
 
     expect(promise).rejects.toThrow(new MissingParamError('email'))
   })
 
   test('Should throw if there\'s no password', async () => {
-    const sut = new AuthUseCase()
+    const { sut } = makeSut()
     const promise = sut.auth('mail.test@mail.com')
 
     expect(promise).rejects.toThrow(new MissingParamError('password'))
   })
 
   test('Should call LoadUserEmailOnRepo with correct email', async () => {
-    class LoadUserEmailOnRepoSpy {
-      async load (email) {
-        this.email = email
-      }
-    }
-
-    const loadUserEmailOnRepoSpy = new LoadUserEmailOnRepoSpy()
-    const sut = new AuthUseCase(loadUserEmailOnRepoSpy)
+    const { sut, loadUserEmailOnRepoSpy } = makeSut()
     await sut.auth('mail.test@mail.com', 'dsdfs')
 
     expect(loadUserEmailOnRepoSpy.email).toBe('mail.test@mail.com')
