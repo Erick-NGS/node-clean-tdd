f8abc9776560a9e1cf4ea76b39b11791efa149ad
Erick Silva <erick.silva@firstdecision.com.br>
2021-05-14 10:40:46 -0300

    moved AuthUseCase class to a separate file

---
+++ src/Domain/usecases/auth-usecase.js
@@ -0,0 +1,31 @@
+const { MissingParamError, InvalidParamError } = require('../../utils/errors')
+
+module.exports = class AuthUseCase {
+  constructor (loadUserEmailOnRepo) {
+    this.loadUserEmailOnRepo = loadUserEmailOnRepo
+  }
+
+  async auth (email, password) {
+    if (!email) {
+      throw new MissingParamError('email')
+    }
+
+    if (!password) {
+      throw new MissingParamError('password')
+    }
+
+    if (!this.loadUserEmailOnRepo) {
+      throw new MissingParamError('loadUserEmailOnRepo')
+    }
+
+    if (!this.loadUserEmailOnRepo.load) {
+      throw new InvalidParamError('loadUserEmailOnRepo')
+    }
+
+    const user = await this.loadUserEmailOnRepo.load(email)
+
+    if (!user) {
+      return null
+    }
+  }
+}
--- src/Domain/usecases/auth-usecase.spec.js
+++ src/Domain/usecases/auth-usecase.spec.js
@@ -1,34 +1,5 @@
 const { MissingParamError, InvalidParamError } = require('../../utils/errors')
-
-class AuthUseCase {
-  constructor (loadUserEmailOnRepo) {
-    this.loadUserEmailOnRepo = loadUserEmailOnRepo
-  }
-
-  async auth (email, password) {
-    if (!email) {
-      throw new MissingParamError('email')
-    }
-
-    if (!password) {
-      throw new MissingParamError('password')
-    }
-
-    if (!this.loadUserEmailOnRepo) {
-      throw new MissingParamError('loadUserEmailOnRepo')
-    }
-
-    if (!this.loadUserEmailOnRepo.load) {
-      throw new InvalidParamError('loadUserEmailOnRepo')
-    }
-
-    const user = await this.loadUserEmailOnRepo.load(email)
-
-    if (!user) {
-      return null
-    }
-  }
-}
+const AuthUseCase = require('./auth-usecase')
 
 const makeSut = () => {
   class LoadUserEmailOnRepoSpy {
