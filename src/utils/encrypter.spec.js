68abee399adfd99735934cb79842829ac89f0c32
Erick Silva <erick.silva@firstdecision.com.br>
2021-05-17 11:34:17 -0300

    added test to check encrypter return, created Encrypter's mock file

--- src/utils/encrypter.spec.js
+++ src/utils/encrypter.spec.js
@@ -1,6 +1,9 @@
+const bcrypt = require('bcrypt')
+
 class Encrypter {
-  async compare (password, hashedPassword) {
-    return true
+  async compare (value, hash) {
+    const isValid = await bcrypt.compare(value, hash)
+    return isValid
   }
 }
 
@@ -11,8 +14,16 @@ const makeSut = () => {
 describe('Encrypter', () => {
   test('Should return true if bcrypt returns true', async () => {
     const sut = makeSut()
-    const isItValid = await sut.compare('any_pass', 'hashed_pass')
+    const isItValid = await sut.compare('any_value', 'hashed_value')
 
     expect(isItValid).toBe(true)
+  })
+
+  test('Should return true if bcrypt returns true', async () => {
+    const sut = makeSut()
+    bcrypt.isValid = false
+    const isItValid = await sut.compare('any_value', 'hashed_value')
+
+    expect(isItValid).toBe(false)
   })
 })
