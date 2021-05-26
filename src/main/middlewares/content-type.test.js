f31352530ac94e8ce3943e9ad4816d3a80323739
Erick Silva <erick.silva@firstdecision.com.br>
2021-05-26 12:44:33 -0300

    created content type test for XML

--- src/main/middlewares/content-type.test.js
+++ src/main/middlewares/content-type.test.js
@@ -3,13 +3,24 @@ const request = require('supertest')
 const app = require('../config/app')
 
 describe('Content-Type middleware', () => {
-  test('Should return JSON Content Type as default', async () => {
+  test('Should return JSON content-type as default', async () => {
     app.get('/content_type_test', (req, res) => {
-      res.send({})
+      res.send('')
     })
 
     await request(app)
       .get('/content_type_test')
       .expect('content-type', /json/)
+  })
+
+  test('Should return XML content-type', async () => {
+    app.get('/content_type_test_xml', (req, res) => {
+      res.type('xml')
+      res.send('')
+    })
+
+    await request(app)
+      .get('/content_type_test_xml')
+      .expect('content-type', /xml/)
   })
 })
