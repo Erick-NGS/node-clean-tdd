c971c7b16e5e95fa319c80dcbcf416f3800db1f5
Erick Silva <erick.silva@firstdecision.com.br>
2021-05-26 10:33:25 -0300

    enabled CORS

--- src/main/config/setup.test.js
+++ src/main/config/setup.test.js
@@ -12,4 +12,16 @@ describe('App setup', () => {
 
     expect(res.headers['x-powered-by']).toBeUndefined()
   })
+
+  test('Should enable CORS', async () => {
+    app.get('/cors_test', (req, res) => {
+      res.send('')
+    })
+
+    const res = await request(app).get('/cors_test')
+
+    expect(res.headers['access-control-allow-origin']).toBe('*')
+    expect(res.headers['access-control-allow-methods']).toBe('*')
+    expect(res.headers['access-control-allow-headers']).toBe('*')
+  })
 })
