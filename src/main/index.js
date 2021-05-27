a3a0e969c5b840902438aae851c411d1fdb65fc3
Erick Silva <erick.silva@firstdecision.com.br>
2021-05-27 12:10:47 -0300

    conn with Mongo before app start listening

--- src/main/index.js
+++ src/main/index.js
@@ -1,3 +1,9 @@
-const app = require('./config/app')
+const MongoHelper = require('../infra/helpers/mongo-helper')
+const env = require('./config/env')
 
-app.listen(3060, () => console.log('Server is up'))
+MongoHelper.connect(env.mongoUrl)
+  .then(() => {
+    const app = require('./config/app')
+    app.listen(3060, () => console.log('Server is up'))
+  })
+  .catch(console.error)
